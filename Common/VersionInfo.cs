3de611e606c3ecd90b246df8106cfcd355f1e8d6
Dmitry Kolomiets <kolomiets@gmail.com>
2018-05-16 00:42:24 +0300

    Add basic build system scripts

---
+++ Common/VersionInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("HazyBits")]
+[assembly: AssemblyCopyright("(C) 2016-(CurrentYear), HazyBits")]
+[assembly: AssemblyProduct("HazyBits TWAIN Cloud")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
